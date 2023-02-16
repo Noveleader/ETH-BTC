8ba983f60621ed45c5f7a63602f36164b2b8ce39
Noveleader <91677627+Noveleader@users.noreply.github.com>
2023-02-16 18:31:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+fetch(
+  "https://www.worldcoinindex.com/apiservice/ticker?key=MYHmBIKDduZOFiLV30ZX4pFfjvee1So7arK&label=ethbtc-ltcbtc&fiat=btc"
+)
+  .then((data) => data.json())
+  .then((cryptoPrices) => {
+    const label = cryptoPrices.Markets[0]["Label"];
+    const cryptoPrice = cryptoPrices.Markets[0]["Price"];
+    const name = cryptoPrices.Markets[0]["Name"];
+    const volume24H = cryptoPrices.Markets[0]["Volume_24h"];
+    const crypto = document.getElementById("crypto");
+    crypto.innerHTML = label + " " + cryptoPrice + " " + volume24H;
+  });
