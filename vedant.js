d13669147dc3a25a64a4e531cfb028554a01726e
Vedant Vatsa <122670026+VedantVatsa@users.noreply.github.com>
2023-03-25 22:51:46 +0530

    Create vedant.js

---
+++ vedant.js
@@ -0,0 +1,14 @@
+fetch(
+  "https://www.worldcoinindex.com/apiservice/ticker?key=MYHmBIKDduZOFiLV30ZX4pFfjvee1So7arK&label=ethbtc-ltcbtc&fiat=btc"
+)
+  .then((data) => data.json())
+  .then((cryptoPrices) => {
+    const label = cryptoPrices.Markets[0]["Label"];
+    const cryptoPrice = cryptoPrices.Markets[0]["Price"];
+    const name = cryptoPrices.Markets[0]["Name"];
+    const volume24H = cryptoPrices.Markets[0]["Volume_24h"];
+    const crypto = document.getElementById("crypto");
+    const vedant = cryptoPrices.Markets[0]["Label"];
+    crypto.innerHTML = `${label} Price:${cryptoPrice} Vol24H:${volume24H}`;
+  
+  });
